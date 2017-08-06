7142e3e9aae34b09562d9edb5816aed58dc8c176
Narek Martikian <Fujman94@gmail.com>
2017-08-07 01:09:26 +0300

    webhook

--- main_app/views.py
+++ main_app/views.py
@@ -311,8 +311,8 @@ def remove_coupon(request, pk):
 
 
 def webhook(request):
-    challenge = request.GET["hub_challenge"]
-    verify_token = request.GET["hub_verify_token"]
+    challenge = request.GET["hub.challenge"]
+    verify_token = request.GET["hub.verify_token"]
     if verify_token == 'abc123':
         return HttpResponse(challenge)
 
